80dde95b51aa9a0a5589ca26410c2edf60e589b0
Pedro Mont <pm2558@nyu.com>
2015-07-21 15:37:46 -0400

    done!

--- Assets/scripts/trigger.cs
+++ Assets/scripts/trigger.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class trigger : MonoBehaviour {
 	
 	bool didITriggerYet = false;
+	public ParticleSystem fireParticles; // assign on inspector
 	
 	// Update is called once per frame
 	void Update () {
@@ -14,6 +15,7 @@ public class trigger : MonoBehaviour {
 	//whenever an object with a rigid body enters the trigger, the code here will execute
 	void OnTriggerEnter() {
 		didITriggerYet = true;
+		fireParticles.Play ();
 	}
 	
 }
