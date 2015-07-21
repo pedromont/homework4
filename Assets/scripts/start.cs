cb800f0793446a529dff4397f6006f01775645ad
Pedro Mont <pm2558@nyu.com>
2015-07-21 11:12:01 -0400

    still missing endgame

---
+++ Assets/scripts/start.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class start : MonoBehaviour {
+	
+		
+		// a public variable is exposed to the inspector and might be changed
+		public float speed = 2f;
+		
+		
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+
+			// move hand
+			if (Input.GetKey (KeyCode.D)) {
+				transform.position += new Vector3 (-1f, 0f, 0f) * Time.deltaTime * speed;
+			}
+
+			
+		}
+	}
---
+++ Assets/scripts/trigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trigger : MonoBehaviour {
+	
+	bool didITriggerYet = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (didITriggerYet == true) {
+			transform.Rotate (3f, 3f, 3f);
+		}
+	}
+	//whenever an object with a rigid body enters the trigger, the code here will execute
+	void OnTriggerEnter() {
+		didITriggerYet = true;
+	}
+	
+}
